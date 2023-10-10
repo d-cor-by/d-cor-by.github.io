b8cfea8fe376bbb8a4e7bc3ad2e0f6890bbb4b42
IgorArt <weblab.imporg@gmail.com>
2023-10-10 16:37:36 +0300

    fix assets buld

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import setupIcons from './components/fontawesome.js'
 import 'glightbox/dist/css/glightbox.css'
 import '/css/app.css'
 
+const photos = import.meta.glob(['/images/photos/full/*'])
+
 document.addEventListener('DOMContentLoaded', function() {
   setupIcons()
 
@@ -11,7 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
     element.innerText = new Date().getFullYear()
   })
 
-  GLightbox({
-    loop: true,
-  })
+  Promise.all(
+    Object.keys(photos).map(path => photos[path]().then((module) => ({ path, url: module.default })))
+  )
+    .then((photos) => {
+      photos.forEach(({ path, url }) => {
+        document.querySelector(`a[data-origin="${path}"]`)?.setAttribute('href', url)
+      })
+
+      GLightbox({
+        loop: true,
+        preload: false,
+      })
+    })
 })
