7b22c17d5d57be66d1ccb559a0e523b549f76237
Weblabio ImpOrg <weblab.imporg@gmail.com>
2025-12-11 14:11:48 +0300

    update location

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: './src/',
-  base: 'https://d-cor.by/',
+  base: 'https://d-cor-by.github.io/',
   build: {
     outDir: '../docs/',
   },
